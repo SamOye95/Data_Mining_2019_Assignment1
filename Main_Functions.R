688976ee47e0e3647d760f88600458fbd420469a
SamOye95 <a.s.m.oyediran@student.utwente.nl>
2019-09-21 18:24:43 +0200

    - Added predict() function to predict the class label for a single attributes input vector
    - Added mkLeaf() function that produces a leaf row.
    
    - Added mkNode() function

--- Main_Functions.R
+++ Main_Functions.R
@@ -42,6 +42,57 @@ tree.grow <- function(x, y, nmin = 0, minleaf = 0, impurity = gini_index){
 #   for each row in x.
 tree.classify <- function (x, tr){
   
+  apply(x,1,predict, tr)
+  
+}
+
+# Function: predict(x, tr)
+# Predicts the class label for a single attributes input vector
+#
+# Arguments:
+#   x : A numerical vector containing the attributes observations
+#   tr : A tree object produced by the tree.grow function
+#
+# Result
+#  The binary class label predicted by the trained tree.
+predict <- function(x, tr){
+  i <- 1
+  while (is.na(tr$label[i]))
+    i <- if (x[tr$splitCol[i]] <= tr$split[i]) tr$left[i] else tr$right[i]
+  return(tr$label[i])
+}
+
+# Function: mkLeaf(y)
+# Proudces a leaf row
+#
+# Arguments
+#   y : A vector of binary class labels
+#
+# Result
+#   A row representing a leaf node in the tree data structure.
+#   All the fields are set to NA, except for label, which is computed applying
+#   majority vote to the class label vector y.
+mkLeaf <- function(y){
+  
+ c(NA, NA, majority_class(y), NA, NA)
+  
 }
 
 
+
+# Function: mkNode(left, right, best)
+# Produces an internal node row.
+#
+# Arguments
+#   left, right : Integer numbers representing the row index at which the left
+#                 and right are stored in the tree data structure.
+#   best : A split object containing the fields split (threshold value) and
+#          index, which is the column number of the attribute matrix in which
+#          the same attribute values are stored
+#
+# Result
+#  A vector representing an internal node row in the tree data structure.
+#  The label field is set to NA.
+mkNode <- function (left.index, right.index, best) {
+  c(left.index, right.index, NA, best$split, best$index)
+}
