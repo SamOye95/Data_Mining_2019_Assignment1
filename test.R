281dee5bf14a8d930f68e65277cba95eec4ebd59
SamOye95 <a.s.m.oyediran@student.utwente.nl>
2019-10-09 08:51:33 +0200

    Cleaned up test functions

--- test.R
+++ test.R
@@ -15,21 +15,6 @@ test.credit <- function(){
   return(credit.actual)
 }
 
-test.eclipse.2 <- function(){
-  eclipse.2 <- read.data("eclipse-metrics-packages-2.0.txt",0, header = TRUE)
-  eclipse.2.without.post <- eclipse.2[,-2]
-  eclipse.2.tree <- tree.grow(eclipse.2.without.post, eclipse.2$post, nmin = 15, minleaf = 5)
-  return(eclipse.2.tree)
-}
-#eclipse.2.actual <- tree.classify(eclipse.2$test.x, eclipse.2.tree)
-#eclipse.2.expected <- eclipse.2$test.y
-#return(table(eclipse.2.expected, eclipse.2.actual))
-testecl3 = function(x, tr){
-  list = tree.classify(x,tr)
-  validation = (as.numeric(eclipse.metrics.packages.3.0$post>0))
-  #retvar = cbind(eclipse.metrics.packages.3.0$post, list)
-  return(table(validation, list))
-}
 
 # Grows a classification tree on the complete pima data set and uses it to
 # predict the training sample itself and finally returns the resulting
@@ -43,3 +28,54 @@ test.pima <- function(){
   pima.expected <- pima$train.y
   return(table(pima.expected, pima.actual))
 }
+
+### SINGLE TREE ###
+
+test.eclipse.2 <- function(){
+  eclipse.2 <- read.data("eclipse-metrics-packages-2.0.txt",0, header = TRUE)
+  eclipse.2.without.post <- eclipse.2[,-2]
+  eclipse.2.tree <- tree.grow(eclipse.2.without.post, eclipse.2$post, nmin = 15, minleaf = 5)
+  return(eclipse.2.tree)
+}
+
+test.eclipse.3 <- function(){
+  eclipse.metrics.packages.3.0 <- read.csv("eclipse-metrics-packages-3.0.csv", sep=";")
+  eclipse.metrics.packages.3.0.1 = eclipse.metrics.packages.3.0[unlist(lapply(eclipse.metrics.packages.3.0,is.numeric))]
+  eclipse.metrics.packages.3.0.numeric = (as.numeric(eclipse.metrics.packages.3.0$post>0))
+  eclipse.metrics.packages.3.0.without.post = (eclipse.metrics.packages.3.0.1[,-2])
+  
+  return(eclipse.metrics.packages.3.0.without.post)
+}
+
+
+#this function creates the confusion confusion matrix table
+testecl3 = function(x, tr){
+  list = tree.classify(x,tr)
+  validation = (as.numeric(eclipse.metrics.packages.3.0$post>0))
+  return(table(validation, list))
+}
+
+#This function returns the confusion for the single tree using eclipse 2.0 as training data 
+# and eclipse 3.0 as testing data 
+test.eclipse.single.tree.cm <- function(){
+  
+  return(testecl3(test.eclipse.3(),test.eclipse.2()))
+}
+
+
+### BAGGING TREE ###
+test.eclipse.2.bag <- function(){
+  eclipse.2 <- read.data("eclipse-metrics-packages-2.0.txt",0, header = TRUE)
+  eclipse.2.without.post <- eclipse.2[,-2]
+  eclipse.bagging.tree <- tree.grow.bag(eclipse.2.without.post, eclipse.2$post, nmin = 15, minleaf = 5,100)
+  return(eclipse.bagging.tree)
+}
+
+
+
+
+
+
+
+
+
